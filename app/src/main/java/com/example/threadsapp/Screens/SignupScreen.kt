0a191891b33c70951087d45301e2146b939d5543
pedro <b_khemari@estin.dz>
2023-09-16 23:22:54 +0100

    fixed some bugs

--- app/src/main/java/com/example/threadsapp/Screens/SignupScreen.kt
+++ app/src/main/java/com/example/threadsapp/Screens/SignupScreen.kt
@@ -2,18 +2,27 @@ package com.example.threadsapp.Screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -39,10 +48,12 @@ import com.example.threadsapp.ui.theme.ThreadsAppTheme
 
 @Composable
 fun SignupScreen(navController: NavController, signupViewModel: SignupViewModel = viewModel()) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .background(Color.White)
+
     ) {
         Column(
             modifier = Modifier
@@ -50,6 +61,9 @@ fun SignupScreen(navController: NavController, signupViewModel: SignupViewModel 
                 .weight(9.5f)
                 .background(Color.White)
                 .padding(28.dp)
+                .verticalScroll(
+                    rememberScrollState()
+                )
 
         ) {
             Icon(
